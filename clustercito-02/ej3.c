4d9c6254686a3e4ef3abaec4826ed7a75c8140fe
Santiago Santana <santana.santiago@gmail.com>
2017-10-23 19:10:38 -0300

    Archivos cluster2

---
+++ clustercito-02/ej3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "mpi.h"
+
+int main (int argc, char** argv)
+{
+	int pid;
+	int size;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &pid);
+   	
+
+		char mensaje_send[4] = "hola";
+		MPI_Bcast(&mensaje_send, 4, MPI_CHAR, 0, MPI_COMM_WORLD);
+		printf("mensaje:\t%s, process id:\t%d, size:\t%d \n", mensaje_send, pid, size);
+
+		
+	
+	MPI_Finalize();
+	return 0;
+}
+
