0000df6b8d3f624a83aceadbf6e8170bd7f06383
Santiago Santana <santana.santiago@gmail.com>
2017-11-23 10:06:56 -0300

    Ejercicio 4

--- clustercito-02/ej4.c
+++ clustercito-02/ej4.c
@@ -3,18 +3,15 @@
 #include <stdlib.h>
 
 int main(int argc, char** argv) {
-
-  const int PING_PONG_LIMIT = 2;
-
-  // Initialize the MPI environment
-  MPI_Init(NULL, NULL);
-  // Find out rank, size
   int world_rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
   int world_size;
+  const int PING_PONG_LIMIT = 15;
+
+  MPI_Init(NULL, NULL);
+
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-  // We are assuming at least 2 processes for this task
   if (world_size != 2) {
     fprintf(stderr, "World size must be two for %s\n", argv[0]);
     MPI_Abort(MPI_COMM_WORLD, 1);
@@ -27,6 +24,7 @@ int main(int argc, char** argv) {
     if (world_rank == ping_pong_count % 2) {
       // Increment the ping pong count before you send it
       ping_pong_count++;
+
       MPI_Send(&ping_pong_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD);
       printf("%d sent and incremented ping_pong_count %d to %d\n",
              world_rank, ping_pong_count, partner_rank);
@@ -39,5 +37,5 @@ int main(int argc, char** argv) {
   }
 
   MPI_Finalize();
-
+  return 0;
 }
