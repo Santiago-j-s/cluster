c4491b4c7745b4040134d3424596e21fc35c554e
Santiago Santana <santana.santiago@gmail.com>
2017-11-23 09:43:07 -0300

    Ejercicio 2

--- clustercito-02/ej2.c
+++ clustercito-02/ej2.c
@@ -6,6 +6,7 @@ int main (int argc, char** argv)
 	int pid;
 	int size;
 	const int rootProcess = 0;
+
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &pid);
@@ -13,11 +14,11 @@ int main (int argc, char** argv)
 	if (pid == rootProcess){
 		char mensaje_send[4] = "hola";	
 		MPI_Send(&mensaje_send, 4, MPI_CHAR, 1, 0, MPI_COMM_WORLD);
-		MPI_Send(&mensaje_send, 4, MPI_CHAR, 2, 0, MPI_COMM_WORLD);		
 	} else {
 		char mensaje_recv[4];
 		MPI_Recv(&mensaje_recv, 4, MPI_CHAR, rootProcess, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		printf("mensaje:\t%s, process id:\t%d, size:\t%d \n", mensaje_recv, pid, size);
+
+		printf("mensaje: %s\nid de proceso: %d\nlongitud de mensaje: %d\n", mensaje_recv, pid, size);
 	}
 	MPI_Finalize();
 	return 0;
