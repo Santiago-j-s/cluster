f42f7982aebed57774faafc1d3533483fcb2a528
Santiago Santana <santana.santiago@gmail.com>
2017-11-23 09:46:11 -0300

    Ejercicio 3

--- clustercito-02/ej3.c
+++ clustercito-02/ej3.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
-#include "mpi.h"
+#include <mpi.h>
 
 int main (int argc, char** argv)
 {
 	int pid;
 	int size;
+
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &pid);
    	
 
-		char mensaje_send[4] = "hola";
-		MPI_Bcast(&mensaje_send, 4, MPI_CHAR, 0, MPI_COMM_WORLD);
-		printf("mensaje:\t%s, process id:\t%d, size:\t%d \n", mensaje_send, pid, size);
+	char mensaje_send[4] = "hola";
+	MPI_Bcast(&mensaje_send, 4, MPI_CHAR, 0, MPI_COMM_WORLD);
 
-		
-	
+	printf("mensaje:\t%s, proceso:\t%d, longitud:\t%d \n", mensaje_send, pid, size);
+
 	MPI_Finalize();
 	return 0;
 }
